6473093132beb6741c774b38e87168a8dd5bdea6
hoang4325 <hoang4325@github.com>
2026-05-12 19:24:49 +0700

    feat: add cart service module skeleton

---
+++ cart-service/src/test/java/com/example/ecommerce/cartservice/CartServiceApplicationTests.java
@@ -0,0 +1,21 @@
+package com.example.ecommerce.cartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = {
+    "eureka.client.enabled=false",
+    "spring.cloud.discovery.enabled=false",
+    "spring.cloud.service-registry.auto-registration.enabled=false",
+    "spring.datasource.url=jdbc:h2:mem:cart_service_context;MODE=PostgreSQL;DB_CLOSE_DELAY=-1",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.hibernate.ddl-auto=create-drop"
+})
+class CartServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
