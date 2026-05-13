2df92e23fa0a93ce218fc9f9403c3756927b33dc
hoang4325 <hoang4325@github.com>
2026-05-13 21:01:55 +0700

    feat: add payment service module skeleton

---
+++ payment-service/src/test/java/com/example/ecommerce/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.ecommerce.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(
+    properties = {
+        "spring.datasource.url=jdbc:h2:mem:payment_context;MODE=PostgreSQL;DATABASE_TO_UPPER=false",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "eureka.client.enabled=false"
+    }
+)
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
